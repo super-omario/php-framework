3b920787f310409e9c543f6bb26c349980d1ad00
super-omario <omar.brahimi.nimda@gmail.com>
2025-08-20 22:59:47 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// framework/index.php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$name = $request->query->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+echo $request->getClientIp();
+
+$response->send();
