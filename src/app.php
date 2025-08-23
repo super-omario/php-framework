53d2baad18913c677a4e27552fb01b5b928af703
super-omario <omar.brahimi.nimda@gmail.com>
2025-08-23 23:18:17 +0200

    Introduce controller and argument resolvers

--- src/app.php
+++ src/app.php
@@ -5,20 +5,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-function is_leap_year(?int $year = null): bool
-{
-    if (null === $year) {
-        $year = (int)date('Y');
-    }
-
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
-
 $routes = new RouteCollection();
 
 $routes->add('hello', new Route('/hello/{name}', [
     'name' => 'World',
-    '_controller' => function (Request $request): Response {
+    '_controller' => 'HelloController::index',
+]));
+
+$routes->add('bye', new Route('/bye', [
+    '_controller' => 'render_template',
+]));
+
+$routes->add('leap_year', new Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => 'LeapYearController::index',
+]));
+
+return $routes;
+
+class HelloController {
+    public function index(Request $request) : Response
+    {
         // $foo will be available in the template
         $request->attributes->set('foo', 'bar');
 
@@ -29,21 +36,34 @@ $routes->add('hello', new Route('/hello/{name}', [
 
         return $response;
     }
-]));
+}
 
-$routes->add('bye', new Route('/bye', [
-    '_controller' => 'render_template',
-]));
-
-$routes->add('leap_year', new Route('/is_leap_year/{year}', [
-    'year' => null,
-    '_controller' => function (Request $request): Response {
-        if (is_leap_year($request->attributes->get('year'))) {
+class LeapYearController
+{
+    public function index(?int $year): Response
+    {
+        if (is_leap_year($year)) {
             return new Response('Yep, this is a leap year!');
         }
 
         return new Response('Nope, this is not a leap year.');
     }
-]));
+}
 
-return $routes;
+function is_leap_year(?int $year = null): bool
+{
+    if (null === $year) {
+        $year = (int)date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+function render_template(Request $request): Response
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
